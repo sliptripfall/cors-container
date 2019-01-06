3f364ca2ddac4dd1fb90488dba1800f395655ef0
Trevor <trevor.m.duncan@gmail.com>
2019-01-05 19:51:22 -0600

    enabling rewrite rules?

--- bootstrap.js
+++ bootstrap.js
@@ -25,7 +25,8 @@ module.exports = function(app){
             uri: requestedUrl,
             resolveWithFullResponse: true,
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
+                'rewrite-urls': true
             }
         })
         .then(originResponse => {            
